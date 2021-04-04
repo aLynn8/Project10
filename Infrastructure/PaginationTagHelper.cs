d597fc40d37eaf4a54be184f1d21fbd719c0ce35
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-04-04 16:35:26 -0600

    Add project files.

---
+++ Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SharpenTheSaw.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpenTheSaw.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-info")]
+
+    public class PaginationTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlInfo;
+
+        public PaginationTagHelper (IUrlHelperFactory uhf)
+        {
+            urlInfo = uhf;
+        }
+
+        public PageNumberingInfo PageInfo { get; set; }
+        public string MealType { get; set; }
+
+        //Build dictionary with our own key value pairs
+        [HtmlAttributeName(DictionaryAttributePrefix ="page-url-")]
+        public Dictionary<string, object> KeyValuePairs { get; set; } = new Dictionary<string, object>();
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            //fulls in URLFACTOR info passedin above
+            IUrlHelper urlHelp = urlInfo.GetUrlHelper(ViewContext);
+
+            TagBuilder FinishedTag = new TagBuilder("div");
+
+            //Build Page options and links
+
+            for (int i = 1; i <= PageInfo.NumPages; i++)
+            {
+                
+                
+                TagBuilder individualTag = new TagBuilder("a");
+
+                KeyValuePairs["pageNum"] = i;
+
+                individualTag.Attributes["href"] = urlHelp.Action("Index", KeyValuePairs);
+                individualTag.InnerHtml.Append(i.ToString());
+
+                FinishedTag.InnerHtml.AppendHtml(individualTag);
+            }
+
+            output.Content.AppendHtml(FinishedTag.InnerHtml);
+        }
+    }
+}
---
+++ Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+//Contains data we pass into Index since we can only pass in one model at a time
+namespace SharpenTheSaw.Models.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<Recipes> Recipes { get; set; }
+        public PageNumberingInfo PageNumberingInfo { get; set; }
+        public string MealCategory { get; set; }
+    }
+}
