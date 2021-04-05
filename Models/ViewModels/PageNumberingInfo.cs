39934da29149d7bbc168267aedb54224e4242148
aLynn8 <61812849+aLynn8@users.noreply.github.com>
2021-04-05 15:29:47 -0600

    fixed error so runs and selected category is hilighted

--- Models/ViewModels/PageNumberingInfo.cs
+++ Models/ViewModels/PageNumberingInfo.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-//Aubrey Farnbach (Wright) Section 2 Group 1
-
 namespace SharpenTheSaw.Models.ViewModels
 {
     public class PageNumberingInfo
